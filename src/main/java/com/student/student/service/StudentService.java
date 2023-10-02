4e6d4588dd8b6127f082e9463d969779e1b99bc4
Ngenge Senior <seniorngenge@gmail.com>
2023-10-01 21:14:08 -0400

    Finished student REST API

--- src/main/java/com/student/student/service/StudentService.java
+++ src/main/java/com/student/student/service/StudentService.java
@@ -2,10 +2,12 @@ package com.student.student.service;
 
 import com.student.student.model.Student;
 import com.student.student.repository.StudentRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -28,6 +30,33 @@ public class StudentService {
             throw new IllegalStateException("Email taken");
         }
         repository.save(student);
+
+    }
+
+    public void deleteStudent(Long studentId) {
+        boolean exists = repository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("Student with id " + studentId + " does not exist");
+        }
+        repository.deleteById(studentId);
+    }
+
+    // Enter a managed state
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = repository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException("Student with id " + studentId + " does not exist"));
+        if (name != null &&
+                !name.isEmpty()
+                && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null &&
+                !email.isEmpty()
+                && !Objects.equals(student.getEmail(), email)) {
+            student.setEmail(email);
+        }
 
     }
 }
